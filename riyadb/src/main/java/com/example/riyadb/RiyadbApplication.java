e550b6318e84a7159dce0436663d190d063ad692
riyaa060 <riyaprol9@gmail.com>
2025-11-28 22:35:52 +0530

    Spring DB

---
+++ riyadb/src/main/java/com/example/riyadb/RiyadbApplication.java
@@ -0,0 +1,13 @@
+package com.example.riyadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiyadbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiyadbApplication.class, args);
+	}
+
+}
---
+++ riyadb/src/test/java/com/example/riyadb/RiyadbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.riyadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiyadbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
